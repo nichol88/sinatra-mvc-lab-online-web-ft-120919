184f43b970da0a5670bb24b5eac4657a9e38f29c
Brian Nicholls <nichol88@gmail.com>
2020-01-29 22:33:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   attr_reader :text
 
   def piglatinize(string)
-    @text = string.doncase
+    @text = string.downcase
     string.split(' ').map { |word| pl_word(word) }.join(' ')
   end
 
